b470b91038ba2f4bc9f66ae0615adc60dd8adb1c
haihai1995 <tranvanhai100195@gmail.com>
2020-09-16 23:43:15 +0700

    cc

--- my-app/src/App.saga.js
+++ my-app/src/App.saga.js
@@ -3,9 +3,11 @@ import { fork } from "redux-saga/effects";
 import homeSaga from "@zef/pages/home/saga";
 import userSaga from "@zef/pages/user/saga";
 // import dropdownSaga from "@zef/pages/user/dropdown/saga";
+import searchSaga from "@zef/pages/user/menu/saga";
 
 export default function* () {
   yield fork(userSaga);
   yield fork(homeSaga);
+  yield fork(searchSaga);
   // yield fork(dropdownSaga);
 }
